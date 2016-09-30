96a2684b17c1344f7c79dd4fc74b12fc0e6d9f36
loic <loic.brigardis@gmail.com>
2016-09-30 14:43:19 +0200

    article selected added

--- assets/js/main.js
+++ assets/js/main.js
@@ -3,7 +3,8 @@ $(document).ready(function() {
     var $nav_mobile_btn = $('.nav-mobile-btn'),
         $nav_mobile = $('.nav-mobile'),
         $nav_mobile_a = $('.nav-mobile ul li a'),
-        $nav_menu_a = $('.nav ul li a');
+        $nav_menu_a = $('.nav ul li a'),
+        $welcome = $('.header-background h1');
 
     $nav_mobile_btn.click(function() {
         $(this).toggleClass('open');
@@ -22,17 +23,29 @@ $(document).ready(function() {
     var $mountain02 = $('.mountain02');
     var $mountain03 = $('.mountain03');
 
+
     $(window).scroll(function() {
 
       var wScroll = $(this).scrollTop();
 
-      $mountain02.css({
-        'transform': 'translate( '+ (-250 + wScroll / 10) +'px , 0px)'
-      });
+      if (wScroll < 300) {
+        var zoom = (1 + (wScroll / 800));
 
-      $mountain03.css({
-        'transform': 'translate( -250px , '+ - wScroll / 10 +'px)'
-      });
+        $mountain02.css({
+          'transform': 'translate( '+ (-250 + wScroll / 10) +'px , 0px)'
+        });
+
+        $mountain03.css({
+          'transform': 'translate( -250px , '+ - wScroll / 10 +'px)'
+        });
+
+        $welcome.css({
+          'transform': 'translateX(-70%) scale('+ zoom +')'
+        });
+
+      }
+
+      console.log(wScroll);
 
     });
 

5a177b9edaf81f9c9648159b1abcf9f0dc45de9e
loic <loic.brigardis@gmail.com>
2016-10-02 20:06:13 +0200

    list items

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,38 +1,41 @@
 $(document).ready(function() {
 
-  var $nav_mobile_btn = $('.nav-mobile-btn'),
-      $nav_mobile = $('.nav-mobile'),
-      $nav_mobile_a = $('.nav-mobile ul li a'),
-      $nav_menu_a = $('.nav ul li a'),
-      $welcome = $('.header-background h1'),
-      $mountain02 = $('.mountain02'),
-      $mountain03 = $('.mountain03'),
-      $close_btn = $('.article-selected-close'),
-      $articles = $('.articles');
-      $article = $('.article');
+    var $nav_mobile_btn = $('.nav-mobile-btn'),
+        $nav_mobile = $('.nav-mobile'),
+        $nav_mobile_a = $('.nav-mobile ul li a'),
+        $nav_menu_a = $('.nav ul li a'),
+        $welcome = $('.header-background h1'),
+        $mountain02 = $('.mountain02'),
+        $mountain03 = $('.mountain03'),
+        $close_btn = $('.article-selected-close'),
+        $articles = $('.articles'),
+        $winSelectArt = $('.article-selected-background'),
+        $article = $('.article');
 
-  $.ajax({
-      url: 'data.json',
-      contentType: "application/json; charset=utf-8",
-      dataType: "json",
-      success: function(data) {
-          var $items = $('.articles');
-          for (var i = 0; i < data.articles.length; i++) {
-              var item = data.articles[i];
-              $items.append(`
-              <div class="article">
+    $winSelectArt.css('height', '0');
+
+    $.ajax({
+        url: 'data.json',
+        contentType: "application/json; charset=utf-8",
+        dataType: "json",
+        success: function(data) {
+            var $items = $('.articles');
+            for (var i = 0; i < data.articles.length; i++) {
+                var item = data.articles[i];
+                $items.append(`
+              <div class="article" artnum="${item.id}">
                 <img src="${item.image}" width="140px" height="140px" alt="${item.altimage}">
                 <p>${item.desc}</p>
                 <div class="price">$${item.price}</div>
               </div>
               `);
-          }
+            }
 
-      },
-      error: function(err) {
-          console.log("erreur" + err);
-      }
-  });
+        },
+        error: function(err) {
+            console.log("erreur" + err);
+        }
+    });
 
     $nav_mobile_btn.click(function() {
         $(this).toggleClass('open');
@@ -65,32 +68,35 @@ $(document).ready(function() {
 
     });
 
-    var $winSelectArt = $('.article-selected-background');
 
+    // $(window).resize(function() {
+    //     $winSelectArt.css({
+    //         'height': '0'
+    //     });
+    // });
 
-    $(window).resize(function() {
-        $winSelectArt.css({
-            'height': '0'
-        });
-    });
+    setTimeout(function() {
 
-    $articles.on('click', '.article' ,function(e) {
-        var target = $(this).parent().index()
-        console.log(target);
+    }, 50);
+
+    $articles.on('click', '.article', function(e) {
+        var target = $(this).parent().index();
         var $heightwinSelectArt = $('.article-selected').height();
-
-        $winSelectArt.css({
-            'height': '' + $heightwinSelectArt + '',
-        });
-
+        var id = $(this).attr('artnum');
         var page = $('.article-selected-background'); // Page cible
         var speed = 750; // Durée de l'animation (en ms)
+        e.preventDefault();
+
+        $.when($.ajax(getSelectedItem(id))).then(function() {
+
+            itemHeight($winSelectArt, $heightwinSelectArt);
+
+        });
 
         $('html, body').animate({
             scrollTop: $(page).offset().top
         }, speed);
 
-        //return false;
 
     });
 
@@ -106,5 +112,43 @@ function toggleActive($menu) {
     $menu.click(function() {
         $menu.removeClass('active');
         $(this).addClass('active');
+    });
+}
+
+function getSelectedItem(id) {
+    $.ajax({
+            url: 'data.json',
+            contentType: "application/json; charset=utf-8",
+            dataType: "json",
+        })
+        .done(function(data) {
+            var artclicked = data.articles[id];
+
+            $('.article-selected').append(`
+            <div class="article-select-focus">
+              <div class="article-photos">
+                <img src="${artclicked.image}" width="240px" height="240px" alt="${artclicked.altimage}">
+              </div>
+            </div>
+            <div class="article-select-desc">
+              <h3>${artclicked.title}</h3>
+              <hr>
+              <p>${artclicked.desc}</p>
+              <span class="price">$${artclicked.price}</span>
+              <hr>
+              <button type="button" name="button">Add to card</button>
+            </div>
+      `);
+
+        })
+        .fail(function() {
+            console.log("error");
+        });
+}
+
+
+function itemHeight($winSelectArt, $heightwinSelectArt) {
+    $winSelectArt.css({
+        'height': '' + $heightwinSelectArt + '',
     });
 }
